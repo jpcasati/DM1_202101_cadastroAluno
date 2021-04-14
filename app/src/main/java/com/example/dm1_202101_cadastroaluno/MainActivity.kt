0225025a0fe617b4e63b15edc7a10e28d2e8ca54
JPCASATI <jpcasati@usp.br>
2021-04-14 20:44:17 -0300

    implementacao de menu na tela principal

--- app/src/main/java/com/example/dm1_202101_cadastroaluno/MainActivity.kt
+++ app/src/main/java/com/example/dm1_202101_cadastroaluno/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.dm1_202101_cadastroaluno
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,5 +33,33 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
 
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.menuLimpar -> {
+                edtNome.text.clear()
+                edtDataNasc.text.clear()
+                edtNome.requestFocus()
+            }
+            R.id.menuHora -> {
+                val horaAtual = SimpleDateFormat("HH:mm:ss").format(Date())
+                Toast.makeText(this, horaAtual, Toast.LENGTH_SHORT).show()
+            }
+            R.id.menuFechar -> finish()
+            else -> Toast.makeText(this, "Ação não programada", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this, "Utilize o menu para fechar", Toast.LENGTH_SHORT).show()
     }
 }
